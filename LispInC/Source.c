c78b210ea2795bf0606c48ea590d6ae62aa9cd55
ido555 <13142010+ido555@users.noreply.github.com>
2020-07-31 23:28:54 +0300

    a

--- LispInC/Source.c
+++ LispInC/Source.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-static char input[2048];
+// (a,b,\0) - null terminator takes 1 index
+// TODO figure out why and how strings longer the specifed buffer do weird stuff?
+// DONE explanation (maybe): any chars that would otherwise overflow 'input'
+// get stored in some sort of system buffer? and then somehow it keeps "feeding" the 'input'
+// buffer until all chars were printed
+static char input[4];
 
 int main(int argc, char** argv) {
-    printf();
     /* Print Version and Exit Information */
-    puts("Lispy Version 0.0.0.0.1");
+    puts("bad Lisp Version 0.00001");
     puts("Press Ctrl+c to Exit\n");
 
     while (1) {
+        // fputs = puts - but without a newline
+        // string, file stream to write to
         fputs("Lisp but bad> ", stdout);
         
         // user string input, max chars to write, file stream to read from;
