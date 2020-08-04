a3293f7a2d5d0db55121f769d08c77c350c699b5
ido555 <13142010+ido555@users.noreply.github.com>
2020-08-04 22:48:34 +0300

    a

--- LCTHW_0-30/ex16_pointers_2/ex16.c
+++ LCTHW_0-30/ex16_pointers_2/ex16.c
@@ -14,14 +14,19 @@ struct Person
 struct Person *Person_Create(char *name, int age ,int height , int weight){
     // malloc - On success, returns the pointer to the beginning of newly allocated memory
     // On failure, returns a null pointer (IMPORTANT remember to catch with assert).
-    struct Person *person = malloc(sizeof(struct Person));
-    // If expression evaluates to TRUE, assert() does nothing
-    // If expression evaluates to FALSE, assert() displays an error message and aborts the program
+    struct Person *person = malloc(sizeof(struct Person)); // assign memory block the size of name+age+height+weight
+    // If TRUE, assert() does nothing
+    // If FALSE, assert() displays an error message and aborts the program
     assert(person != NULL);
     // TODO fix current understanding of pointers if needed
-    // my (ok-ish) explanation here
     // person->name is equivalent to (*person).name 
-    // *person is pointing to 0xb73eb000 in memory  and *(person).name is 0xb73eb000+offset
+    // *person is pointing to 0xb73eb000(example address) in memory  and *(person).name is 0xb73eb000+offset
+
+    // each "cell" in memory is 1 byte
+    // and the offset for height for example would be - sizeof(name)+sizeof(age) - (for example: 1 byte for char + 4 bytes for int)
+    // so 0xb73eb000+5 = height
+
+    
     person->name = strdup(name);
     person->age = age;
     person->height = height;
