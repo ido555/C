f7eb2302ee9b9013f27f0cdb2e6a2c0c24c48cb3
ido555 <13142010+ido555@users.noreply.github.com>
2020-08-04 19:35:41 +0300

    a

--- LCTHW_0-30/ex14-stdio_string_ctype_bool_functions_forwardDeclerations/ex14.c
+++ LCTHW_0-30/ex14-stdio_string_ctype_bool_functions_forwardDeclerations/ex14.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-// forward declarations - declaration of an identifier 
+// forward declarations - declaration of an identifier
 // (for function, variable, etc...) for which the programmer has not yet given a complete definition
-// required for a compiler to know certain properties of an identifier 
-int isValid(char ch);
+// required for a compiler to know certain properties of an identifier
+bool isValid(char ch);
 void print_letters(char str[]);
-
 
 void print_letters(char str[])
 {
     printf("sizeof str is: %d\n", strlen(str));
-    for (int i = 0; i < strlen(str); i++){
+    for (int i = 0; i < strlen(str); i++)
+    {
         if (isValid(str[i]))
         {
             putc(str[i], stdout);
         }
-        
     }
-    
 }
 
-int isValid(char ch)
+bool isValid(char ch)
 {
+    // ctype used here
     return isalpha(ch) || isblank(ch);
 }
 
